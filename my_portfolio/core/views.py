b74976d72d59697634e1d874c7be43c1e0347954
syny-o <synek.o@seznam.cz>
2024-12-15 11:58:26 +0100

    email support

--- my_portfolio/core/views.py
+++ my_portfolio/core/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import JsonResponse
+from django.core.mail import send_mail
 
 from .models import Contact
 from .api.serializers import ContactSerializer
@@ -18,11 +19,24 @@ def receive_contact_from_api(request):
 
     if request.method == 'POST':
 
-        print('TEST' + str(request.POST))
 
         serializer = ContactSerializer(data=request.POST)
         if serializer.is_valid():
             serializer.save()
+        
+            # Extract concrete values from the serializer
+            message = serializer.validated_data.get('message', 'No message provided')
+            email = serializer.validated_data.get('email', 'No email provided')
+            name = serializer.validated_data.get('name', 'Anonymous')
+
+            # Send the email
+            send_mail(
+                subject=f'{name} sent you a message on your web portfolio',
+                message=message,
+                from_email=email,
+                recipient_list=['synek.o@seznam.cz', 'synekjbc@gmail.com'],
+            )
+
             return JsonResponse(serializer.data, status=201)
         
         return JsonResponse(serializer.errors, status=400)
--- my_portfolio/my_portfolio/settings/base.py
+++ my_portfolio/my_portfolio/settings/base.py
@@ -11,6 +11,12 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv
+
+
+# Load environment variables from .env file
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
@@ -107,6 +113,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
@@ -114,6 +121,14 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
-import os
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+
+# Email settings
+
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = "synekjbc@gmail.com"
+EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
